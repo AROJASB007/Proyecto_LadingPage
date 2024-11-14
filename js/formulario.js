d6320ea37b4941458a04ec8c280f49f068503a40
AROJASB007 <aleroba15@gmail.com>
2024-11-14 14:05:22 -0500

    Formulario Dinamico con javascript

--- js/formulario.js
+++ js/formulario.js
@@ -1,15 +1,19 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
- 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
- 
-    const responseDiv = document.getElementById('response');
-    responseDiv.innerHTML = `
-        <p>¡Gracias por contactarnos, ${name}!</p>
-        <p>Hemos recibido tu mensaje y te responderemos a ${email} lo antes posible.</p>
-    `;
- 
-    document.getElementById('contactForm').reset();
-});
+// Permitir solo letras en el nombre
+document.getElementById('name').addEventListener('input', function (e) {
+    this.value = this.value.replace(/[^a-zA-Z\s]/g, '');
+  });
+  
+  // Definir caracteres no permitidos en el correo
+  document.getElementById('email').addEventListener('input', function (e) {
+    this.value = this.value.replace(/[!#$%^&*()+=\[\]{};':"\\|,<>\/?]+/g, '');
+  });
+  
+  // Contar caracteres en el mensaje
+  document.getElementById('message').addEventListener('input', function (e) {
+    const maxLength = 500;
+    const currentLength = this.value.length;
+    document.getElementById('charCount').textContent = `${currentLength}/${maxLength}`;
+    if (currentLength > maxLength) {
+      this.value = this.value.substring(0, maxLength);
+    }
+  });
